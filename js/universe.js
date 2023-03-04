ef18de85856fbdf1f57aeb76e3f5b0574fd1edb9
Masum <masumt999.com>
2023-03-04 23:42:48 +0600

    all checked and some html,css updated

--- js/universe.js
+++ js/universe.js
@@ -1,73 +1,107 @@
 // all data loader here .API
-const loadAllData = (defaultCard) =>{
-    loadSpinner(true);
-    fetch('https://openapi.programming-hero.com/api/ai/tools')
-    .then(res => res.json())
-    .then(data =>{
-      allData=data.data.tools;
-      displayCard(data.data.tools,defaultCard);
-      
-    })
-   
-}
+const loadAllData = (defaultCard) => {
+  loadSpinner(true);
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
+    .then((data) => {
+      allData = data.data.tools;
+      displayCard(data.data.tools, defaultCard);
+    });
+};
 
-const displayCard = (allCard,defaultCard) =>{
-    // console.log(allCard)
-    const allCardContainer = document.getElementById('card-container');
-    allCardContainer.innerHTML = '';
+const displayCard = (allCard, defaultCard) => {
+  // console.log(allCard)
+  const allCardContainer = document.getElementById("card-container");
+  allCardContainer.innerHTML = "";
 
-    if (defaultCard && allCard.length > 6) {
-      allCard = allCard.slice(0, 6);
-      document.getElementById("see-more-btn").classList.remove("d-none");
-    }
+  if (defaultCard && allCard.length > 6) {
+    allCard = allCard.slice(0, 6);
+    document.getElementById("see-more-btn").classList.remove("d-none");
+  }
 
-   
-    allCard.forEach(card =>{
-      // console.log(card.id)
-        const cardDiv= document.createElement('div');
-        cardDiv.classList.add('col');
-        cardDiv.innerHTML =`
+  allCard.forEach((card) => {
+    // console.log(card.id)
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("col");
+    cardDiv.innerHTML = `
         
         <div class="card h-100">
             <img src="${card.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
                 <ol class="card-features">
-                   ${card.features.map(feature => `<li>${feature}</li>`).join('')}
+                   ${card.features
+                     .map((feature) => `<li>${feature}</li>`)
+                     .join("")}
                 </ol>
             </div>
              <div class="card-footer d-flex justify-content-between align-items-center">
                 <div>
                 <h6>${card.name}</h6>
-                <p><i class="fa-solid fa-calendar-days"></i> ${card.published_in}</p>
+                <p><i class="fa-solid fa-calendar-days"></i> ${
+                  card.published_in
+                }</p>
                 </div>
-                <div>
-                <button onclick ="loadCardDetails('${card.id}')" type="button" class="btn btn-info rounded-circle" data-bs-toggle="modal" data-bs-target="#cardDetails"><i class="fa-solid fa-arrow-right"></i></button>
+                <div class="">
+                <button onclick ="loadCardDetails('${
+                  card.id
+                }')" type="button" class="card-btn btn btn-info rounded-circle " data-bs-toggle="modal" data-bs-target="#cardDetails"><i class="fa-solid fa-arrow-right"></i></button>
                 </div>
 
              </div>
         </div>
         
         `;
-        allCardContainer.appendChild(cardDiv);
-        loadSpinner(false);
-    })
-}
+    allCardContainer.appendChild(cardDiv);
+    loadSpinner(false);
+  });
+};
+
+// show all data by see more button
+const seeMoreData = () => {
+  loadAllData();
+  loadSpinner(true);
+  document.getElementById("see-more-btn").classList.add("d-none");
+};
+// spinner/loading
+const loadSpinner = (value) => {
+  const spinner = document.getElementById("loader");
+  if (value) {
+    spinner.classList.remove("d-none");
+  } else {
+    spinner.classList.add("d-none");
+  }
+};
 
 // Cards details data load here
-const loadCardDetails  = (id) =>{
+function loadCardDetails(id) {
   fetch(` https://openapi.programming-hero.com/api/ai/tool/${id}`)
-  .then(res => res.json())
-  .then(data =>detailsCard(data.data))
+    .then((res) => res.json())
+    .then((data) => detailsCard(data.data));
 }
-
-const detailsCard = cardDetails =>{
-  const detailsCardContainer = document.getElementById('card-details-container');
-  detailsCardContainer.innerHTML = '';
-  const { description, image_link, input_output_examples, pricing, accuracy, integrations, features } = cardDetails;
-      const detailsDiv = document.createElement('div');
-      detailsDiv.classList.add('modal-body',  'd-flex', 'flex-column',  'flex-md-row', 'gap-2');
-      detailsDiv.innerHTML = `
+const detailsCard = (cardDetails) => {
+  const detailsCardContainer = document.getElementById(
+    "card-details-container"
+  );
+  detailsCardContainer.innerHTML = "";
+  const {
+    description,
+    image_link,
+    input_output_examples,
+    pricing,
+    accuracy,
+    integrations,
+    features,
+  } = cardDetails;
+  const detailsDiv = document.createElement("div");
+  detailsDiv.classList.add(
+    "modal-body",
+    "d-flex",
+    "flex-column",
+    "flex-md-row",
+    "gap-2"
+  );
+  detailsDiv.innerHTML = `
       <div class="border border-danger rounded p-2 card-details">
       <div>
         <h6>${description}</h6>
@@ -90,81 +124,80 @@ const detailsCard = cardDetails =>{
         <div>
           <h6>Features</h6>
           <ul>
-          <li>${features ? features[1].feature_name : "no found feature"}</li> 
-          <li>${features ? features[2].feature_name : "no found feature"}</li> 
-          <li>${features ? features[3].feature_name : "no found feature"}</li> 
-          
-
+          ${modalFeature(features)}
           </ul>
         </div>
         <div>
           <h6>Integrations</h6>
           <ul>
-         ${integrationsShow(integrations)}
+          ${integrationsShow(integrations)}
+        
           </ul>
         </div>
       </div>
     </div>
     <div class="border  rounded p-2 ">
+        <div class="accuracy-btn">
+        ${accuracyDetails(accuracy)}
+        </div>
        <div>
-         <div class="accuracy-btn">
-            ${accuracy.score * 100 > 80 ? '<button class="btn btn-info text-white"> accuracy ' + accuracy.score * 100 + '%</button>' : '<button class="btn btn-warning text-white">accuracy Low</button>'}
-          </div>
         <img src="${image_link[0]}" alt="..." class="w-100 rounded">
        </div>
        <div class="mt-4 text-center">
-         <h6>${input_output_examples ? input_output_examples[0].input : "Can you give any example?"}</h6>
-        <p class="mt-3">${input_output_examples ? input_output_examples[0].output : "No! Not Yet! Take a break!!!"}</p>
+         <h6>${
+           input_output_examples
+             ? input_output_examples[0].input
+             : "Can you give any example?"
+         }</h6>
+        <p class="mt-3">${
+          input_output_examples
+            ? input_output_examples[0].output
+            : "No! Not Yet! Take a break!!!"
+        }</p>
        </div>
     </div>
       `;
-      detailsCardContainer.appendChild(detailsDiv);
-  }
-  
-// show all data by see more button start here
-  const seeMoreData = ()=>{
-    loadAllData();
-    loadSpinner(true);
-    document.getElementById("see-more-btn").classList.add("d-none");
-
-  }
-  loadCardDetails();
-
-
-// spinner/loading start here
-const loadSpinner = (value)=>{
- const spinner= document.getElementById("loader");
-  if(value){
-    spinner.classList.remove("d-none");
-  }
-  else{
-    spinner.classList.add("d-none");
-
-  }
-}
-
-// show integrations start here
-const integrationsShow = (integrations) => {
-  let integrationsValue = '';
+  detailsCardContainer.appendChild(detailsDiv);
+};
+// show all integrations in modal
+function integrationsShow(integrations) {
+  let integrationsValue = "";
   if (integrations === null) {
-      return `<li>No items found</li>`;
-  }
-  else {
-    integrations.forEach(integrations => {
-      integrationsValue += `<li>${integrations ? integrations : "no items found"}</li>`;
-      });
-      return integrationsValue;
+    return `<li>No items found</li>`;
+  } else {
+    integrations.forEach((integrations) => {
+      integrationsValue += `<li>${integrations}</li>`;
+    });
+    return integrationsValue;
   }
 }
+// show all modal feature
+const modalFeature = (features) => {
+  let modalFeatureHtml = "";
+  for (const value in features) {
+    modalFeatureHtml += `<li>${features[value].feature_name}</li>`;
+  }
+  return modalFeatureHtml;
+};
+// show all data Sort By Date button
+const showDateTime = () => {
+  loadSpinner(true);
+  const all = allData.sort(function (a, b) {
+    const dateA = new Date(a.published_in);
+    const dateB = new Date(b.published_in);
+    return dateB - dateA;
+  });
+  document.getElementById("see-more-btn").classList.add("d-none");
+  displayCard(all);
+};
 
-// show all data Sort By Date button start
-const showDateTime = () =>{
-loadSpinner(true);
-const all = allData.sort(function(a,b){
- const dateA = new Date(a.published_in);
- const dateB = new Date(b.published_in);
- return dateB - dateA; 
-});
-document.getElementById("see-more-btn").classList.add("d-none");
-displayCard(all);
-}
+// show accuracy
+const accuracyDetails = (accuracy) => {
+  let accuracyHtml = "";
+  if (accuracy.score !== null) {
+    accuracyHtml = `<button class="btn btn-warning text-white">${
+      accuracy.score * 100
+    }% accuracy</button>`;
+  }
+  return accuracyHtml;
+};
